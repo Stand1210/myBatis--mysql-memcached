a2e9de274670d56056a71c7cd91a864505b2d81d
keqi1 <keqi1@bj-m-207617a.local>
2017-07-18 17:42:57 +0800

    Users

--- src/main/java/com/weibo/api/users/controller/Users.java
+++ src/main/java/com/weibo/api/users/controller/Users.java
@@ -1,5 +1,9 @@
 package com.weibo.api.users.controller;
 
+import cn.sina.api.commons.cache.driver.VikaCacheClient;
+import cn.sina.api.commons.util.JsonBuilder;
+import com.weibo.api.users.dao.UsersDao;
+import com.weibo.api.users.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -8,10 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.weibo.api.users.dao.UsersDao;
-
-import cn.sina.api.commons.cache.driver.VikaCacheClient;
 
 @Controller
 @EnableAutoConfiguration
@@ -34,18 +34,43 @@ public class Users {
     
     @RequestMapping(value = "/add.json", method = RequestMethod.POST)
     @ResponseBody
-    public String usersAdd(@RequestParam("name") String name, 
+    public String usersAdd(@RequestParam("id") long id,
+                           @RequestParam("name") String name,
                            @RequestParam("age") int age,
                            @RequestParam("gender") String gender) {
-        // TODO Auto-generated method stub        
-        return null;
+        // TODO Auto-generated method stub
+        if (usersShow(id) != null) {
+            return "user already exits!";
+        }
+        User user = new User();
+        user.setId(id);
+        user.setName(name);
+        user.setAge(age);
+        user.setGander(gender);
+        JsonBuilder jsonBuilder = new JsonBuilder();
+        if (usersDao.add(id, name, age, gender) && usersCache.add(String.valueOf(id), user)) {
+            jsonBuilder.append("uid", id)
+                       .append("name", name)
+                       .append("age", age)
+                       .append("gender", gender);
+        } else {
+            jsonBuilder.append("error", "userAdd fail");
+        }
+        return jsonBuilder.flip().toString();
     }
     
     @RequestMapping(value = "/delete.json", method = RequestMethod.POST)
     @ResponseBody
     public String usersDelete(@RequestParam("id") long id) {
         // TODO Auto-generated method stub
-        return null;
+        if (usersShow(id) == null) {
+            return "user not exits!";
+        }
+
+        if (usersDao.delete(id) && usersCache.delete(String.valueOf(id))) {
+            return "{*true}";
+        }
+        return "{*false}";
     }
     
     @RequestMapping(value = "/modify.json", method = RequestMethod.POST)
@@ -55,14 +80,48 @@ public class Users {
                               @RequestParam("age") int age,
                               @RequestParam("gender") String gender) {
         // TODO Auto-generated method stub
-        return null;
+        if (usersShow(id) == null) {
+            return "user not exits!";
+        }
+        User user = new User();
+        user.setId(id);
+        user.setName(name);
+        user.setAge(age);
+        user.setGander(gender);
+        JsonBuilder jsonBuilder = new JsonBuilder();
+        if (usersDao.update(id, name, age, gender) && usersCache.set(String.valueOf(String.valueOf(id)), user)) {
+            jsonBuilder.append("uid", id)
+                       .append("name", name)
+                       .append("age", age)
+                       .append("gender", gender);
+        } else {
+            jsonBuilder.append("error", "modify fail");
+        }
+        return jsonBuilder.flip().toString();
     }
     
     @RequestMapping(value = "/show.json", method = RequestMethod.GET)
     @ResponseBody
     public String usersShow(@RequestParam("id") long id) {
         // TODO Auto-generated method stub
-        return null;
+        JsonBuilder jsonBuilder = new JsonBuilder();
+        String userString = usersCache.get(String.valueOf("id")).toString();
+        User user = null;
+        if (userString == null) {
+            user = usersDao.show(id);
+            if (user == null) {
+                return null;
+            }
+            usersCache.add(String.valueOf(id), user);
+        } else {
+            return userString;
+        }
+
+        jsonBuilder.append("uid", user.getId())
+                    .append("name", user.getName())
+                    .append("age", user.getAge())
+                    .append("gender", user.getGender());
+        return jsonBuilder.flip().toString();
     }
     
     public static void main(String[] args) throws Exception {
